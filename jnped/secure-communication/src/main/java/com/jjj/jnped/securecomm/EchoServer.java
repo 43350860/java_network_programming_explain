7d2034a028a4cc94c8427a0c83171462af99ad0d
43350860 <jiangjiajia1103@gmail.com>
2014-10-13 17:46:23 +0800

    增加EchoServer代码

---
+++ jnped/secure-communication/src/main/java/com/jjj/jnped/securecomm/EchoServer.java
@@ -0,0 +1,105 @@
+package com.jjj.jnped.securecomm;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+/**
+ * server端程序
+ * @author jm
+ */
+public class EchoServer {
+
+  private static final Logger LOGGER = Logger.getLogger("EchoServer");
+  private final static int PORT = 8000;
+  private static final char PASSPHRASE[] = "654321".toCharArray();
+  
+  private SSLServerSocket sslServerSocket;
+  private String keyPath;
+  
+  public EchoServer() throws Exception {
+    
+    System.setProperty("javax.net.debug", "all");
+    
+    keyPath = EchoServer.class.getClassLoader().getResource("test.keys").getPath();
+    //创建ssl socket 上下文对象
+    SSLContext sslContext = createSSLSocketContext();
+    //创建一个socket
+    SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
+    sslServerSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(PORT);
+    LOGGER.info("服务启动完成！");
+    LOGGER.info("是否使用客户端模式？"+sslServerSocket.getUseClientMode());
+    LOGGER.info("是否需要客户端提交证书？"+sslServerSocket.getNeedClientAuth());
+    
+    String[] supportedCipherSuites = sslServerSocket.getSupportedCipherSuites();
+    sslServerSocket.setEnabledCipherSuites(supportedCipherSuites);
+  }
+  
+  public SSLContext createSSLSocketContext() throws Exception {
+    //加载证书
+    KeyStore ks = KeyStore.getInstance("JKS");
+    ks.load(new FileInputStream(keyPath), PASSPHRASE);
+    
+    //证书安全管理
+    KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+    kmf.init(ks, PASSPHRASE);
+    
+    //用于验证对方证书的管理类
+    TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+    tmf.init(ks);
+    
+    SSLContext sslContext = SSLContext.getInstance("SSL");
+    sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+    return sslContext;
+  }
+  
+  private String echo(String msg) {
+    return "echo:"+msg;
+  }
+  
+  private PrintWriter getWriter(Socket socket) throws IOException {
+    return new PrintWriter(socket.getOutputStream(),true);
+  }
+  
+  private BufferedReader getReader(Socket socket) throws IOException {
+    return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  }
+  
+  private void service() {
+    while ( true ) {
+      Socket socket = null;
+      try {
+        socket = sslServerSocket.accept();
+        LOGGER.info("收到一个新的连接。Host:  "+socket.getInetAddress()+", 端口:"+socket.getPort());
+        
+        BufferedReader reader = getReader(socket);
+        PrintWriter writer = getWriter(socket);
+        
+        for (String content = null;  null != (content=reader.readLine());) {
+          LOGGER.info(content);
+          writer.println(echo(content));
+        }
+      } catch (Exception e) {
+        LOGGER.log(Level.INFO, "", e);
+      }
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+    EchoServer server = new EchoServer();
+    server.service();
+  }
+  
+}
--- jnped/secure-communication/src/main/java/com/jjj/jnped/securecomm/HttpsClient.java
+++ jnped/secure-communication/src/main/java/com/jjj/jnped/securecomm/HttpsClient.java
@@ -4,16 +4,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
 
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.net.ssl.HandshakeCompletedListener;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
 public class HttpsClient {
   
   static final Logger LOGGER = Logger.getLogger("HttpsClient");
-  String host = "www.baidu.com";
+  String host = "23.59.134.219";//www.usps.com
   int port = 443;
   SSLSocketFactory mSSLSocketFactory;
   SSLSocket mSSLSocket ;
@@ -21,21 +24,37 @@ public class HttpsClient {
   public void createSoket() throws UnknownHostException, IOException {
     //首先获取SSLSocketFactory
     mSSLSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    LOGGER.info("mSSLSocketFactory:"+mSSLSocketFactory.getClass().getName());
     //在通过factory获取SSLSocket对象
     mSSLSocket = (SSLSocket) mSSLSocketFactory.createSocket(host, port);
+    mSSLSocket.setSoTimeout(10000);
+    LOGGER.info("mSSLSocket:"+mSSLSocket.getClass().getName());
+    InetAddress inetAddress = mSSLSocket.getInetAddress();
+    LOGGER.info("连接上："+inetAddress.toString());
     //通过SSLSocket取到当前支持的所有加密的方式
     String[] enabledCipherSuites = mSSLSocket.getSupportedCipherSuites();
     //设置当前socket支持的加密方式
     mSSLSocket.setEnabledCipherSuites(enabledCipherSuites);
+    mSSLSocket.addHandshakeCompletedListener(new HandshakeCompletedListener() {
+      
+      @Override
+      public void handshakeCompleted(HandshakeCompletedEvent event) {
+        LOGGER.info("握手结束.");
+        LOGGER.info("加密套件："+event.getCipherSuite());
+        LOGGER.info("会话为："+event.getSession());
+        LOGGER.info("通信对方为："+event.getSession().getPeerHost());
+      }
+    });
+    
     //随便打个日志
-    printLog(enabledCipherSuites);
+    //printLog(enabledCipherSuites);
   }
   
   
   public void communicate() throws IOException {
     //发送数据
     StringBuilder httpProtocolBuilder = new StringBuilder();
-    httpProtocolBuilder.append("GET http://"+host+" HTTP/1.1 \r\n");
+    httpProtocolBuilder.append("GET https://"+host+" HTTP/1.1 \r\n");
     httpProtocolBuilder.append("Host:"+host+"\r\n");
     httpProtocolBuilder.append("Accept:*/*\r\n");
     httpProtocolBuilder.append("\r\n");
